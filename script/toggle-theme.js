c8898cea8404d2b969b8b04df5288f8265849c33
linux-neoros <muhammadsalama909@gmail.com>
2022-10-19 22:54:29 +0700

    Fixed dark mode

--- script/toggle-theme.js
+++ script/toggle-theme.js
@@ -1,9 +1,16 @@
 var body = document.body;
 var switching = document.getElementById('switch');
-var toggle_btn = document.getElementById('toggle');
+var nr_icons = document.getElementById('nr-icons');
 function toggle(){
-    body.classList.toggle('dark-mode');
-    toggle_btn.classList.toggle('nr-switch-active');
+    if(body.classList.contains('dark-mode')){
+        body.classList.remove('dark-mode');
+        nr_icons.innerHTML = '<i class="fa-solid fa-sun"></i>';
+        nr_icons.classList.remove('is-dark-mode');
+    } else {
+        body.classList.toggle('dark-mode');
+        nr_icons.innerHTML = '<i class="fa-solid fa-moon"></i>';
+        nr_icons.classList.toggle('is-dark-mode');
+    }
 }
 
 switching.addEventListener('click', toggle);
