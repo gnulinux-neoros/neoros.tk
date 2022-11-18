c0b2b2f0fa98030b9653304df4423196584c4dc0
linux-neoros <muhammadsalama909@gmail.com>
2022-11-18 17:18:20 +0700

    https://neoros.tk

--- script/index.js
+++
@@ -1,8 +0,0 @@
-var smalltext = document.querySelector('#nr-smalltext');
-
-setTimeout(() => {
-    if(smalltext.style.opacity == '0'){
-        smalltext.classList.add('nr-animation');
-        smalltext.style.opacity = '1';
-    }
-}, 400);
